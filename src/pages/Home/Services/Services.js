75342e5ab9b4990c8b58bf8722b2ce703de7444b
Anas <anasmahmud523@gmail.com>
2022-11-12 02:28:45 +0600

    module 70-5

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [isAsc, setIsAsc] = useState(true);
+    const [search, setSearch] = useState('');
+    const searchRef = useRef();
+
     useEffect(() => {
-        fetch('https://genius-car-server-kappa-khaki.vercel.app/services')
+        fetch(`http://localhost:5000/services?search=${search}&order=${isAsc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data))
-    }, [])
+    }, [isAsc, search]);
+
+    const handleSearch = () => {
+        setSearch(searchRef.current.value);
+    }
 
     return (
         <div>
@@ -16,6 +25,9 @@ const Services = () => {
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised  <br />
                     words which don't look even slightly believable. </p>
+                <input className='input input-sm mx-3' ref={searchRef} type="text" />
+                <button onClick={handleSearch}>Search</button>
+                <button onClick={() => setIsAsc(!isAsc)} className="btn btn-ghost mx-4">{isAsc ? 'desc' : 'asc'}</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
